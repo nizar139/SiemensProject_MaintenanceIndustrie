01611f6e9cc273ed6d72b02c906bd6bef61d5444
Nizar <nizar.elghazal@student-cs.fr>
2023-05-09 18:18:18 +0200

    model using past data and model for each speed

--- utility.py
+++ utility.py
@@ -4,6 +4,12 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 
+def shift_df(df):
+    df_shifted = df.shift(1)
+    df_shifted.iloc[0] = df.iloc[0]
+    return df_shifted.drop(df_shifted.index[-1])
+
+
 def read_data(file_name):
     ''' 
     This subfunction reads the training data from the original excel file, and transform it into a dataframe.
@@ -12,61 +18,121 @@ def read_data(file_name):
     Output:
     * df_data: A dataframe containing the data.
     '''
-    tmp_df_ok_100 = pd.read_excel(file_name, sheet_name=0, usecols='C:N', header=3, nrows=30)
+    print('test')
+    tmp_df_ok_100 = pd.read_excel(
+        file_name, sheet_name=0, usecols='C:N', header=3, nrows=30)
     tmp_df_ok_100['Label'] = 0
     tmp_df_ok_100['Speed'] = 100
 
-    tmp_df_ok_500 = pd.read_excel(file_name, sheet_name=0, usecols='C:N', header=38, nrows=30)
+    tmp_df_ok_500 = pd.read_excel(
+        file_name, sheet_name=0, usecols='C:N', header=38, nrows=30)
     tmp_df_ok_500['Label'] = 0
     tmp_df_ok_500['Speed'] = 500
 
-    tmp_df_ok_1000 = pd.read_excel(file_name, sheet_name=0, usecols='C:N', header=72, nrows=30)
+    tmp_df_ok_1000 = pd.read_excel(
+        file_name, sheet_name=0, usecols='C:N', header=72, nrows=30)
     tmp_df_ok_1000['Label'] = 0
     tmp_df_ok_1000['Speed'] = 1000
 
-    tmp_df_ko_100 = pd.read_excel(file_name, sheet_name=1, usecols='C:N', header=3, nrows=30)
+    tmp_df_ko_100 = pd.read_excel(
+        file_name, sheet_name=1, usecols='C:N', header=3, nrows=30)
     tmp_df_ko_100['Label'] = 1
     tmp_df_ko_100['Speed'] = 100
 
-    tmp_df_ko_500 = pd.read_excel(file_name, sheet_name=1, usecols='C:N', header=37, nrows=30)
+    tmp_df_ko_500 = pd.read_excel(
+        file_name, sheet_name=1, usecols='C:N', header=37, nrows=30)
     tmp_df_ko_500['Label'] = 1
     tmp_df_ko_500['Speed'] = 500
 
-    tmp_df_ko_1000 = pd.read_excel(file_name, sheet_name=1, usecols='C:N', header=80, nrows=30)
+    tmp_df_ko_1000 = pd.read_excel(
+        file_name, sheet_name=1, usecols='C:N', header=80, nrows=30)
     tmp_df_ko_1000['Label'] = 1
     tmp_df_ko_1000['Speed'] = 1000
 
-    df_data_org = pd.concat([tmp_df_ok_100, tmp_df_ok_500, tmp_df_ok_1000, tmp_df_ko_100, tmp_df_ko_500, tmp_df_ko_1000], ignore_index=True)
+    df_data_org = pd.concat([tmp_df_ok_100, tmp_df_ok_500, tmp_df_ok_1000,
+                            tmp_df_ko_100, tmp_df_ko_500, tmp_df_ko_1000], ignore_index=True)
 
     return df_data_org
+
+
+# def read_data_shifted(file_name):
+#     '''
+#     This subfunction reads the training data from the original excel file, and transform it into a dataframe.
+#     Input:
+#     * file_name: A string of the filename.
+#     Output:
+#     * df_data: A dataframe containing the data.
+#     '''
+#     tmp_df_ok_100 = pd.read_excel(
+#         file_name, sheet_name=0, usecols='C:N', header=3, nrows=30)
+#     tmp_df_ok_100['Label'] = 0
+#     tmp_df_ok_100['Speed'] = 100
+#     tmp_df_ok_100_shifted = shift_df(tmp_df_ok_100)
+
+#     tmp_df_ok_500 = pd.read_excel(
+#         file_name, sheet_name=0, usecols='C:N', header=38, nrows=30)
+#     tmp_df_ok_500['Label'] = 0
+#     tmp_df_ok_500['Speed'] = 500
+#     tmp_df_ok_500_shifted = shift_df(tmp_df_ok_500)
+
+#     tmp_df_ok_1000 = pd.read_excel(
+#         file_name, sheet_name=0, usecols='C:N', header=72, nrows=30)
+#     tmp_df_ok_1000['Label'] = 0
+#     tmp_df_ok_1000['Speed'] = 1000
+#     tmp_df_ok_1000_shifted = shift_df(tmp_df_ok_1000)
+
+#     tmp_df_ko_100 = pd.read_excel(
+#         file_name, sheet_name=1, usecols='C:N', header=3, nrows=30)
+#     tmp_df_ko_100['Label'] = 1
+#     tmp_df_ko_100['Speed'] = 100
+#     tmp_df_ko_100_shifted = shift_df(tmp_df_ko_100)
+
+#     tmp_df_ko_500 = pd.read_excel(
+#         file_name, sheet_name=1, usecols='C:N', header=37, nrows=30)
+#     tmp_df_ko_500['Label'] = 1
+#     tmp_df_ko_500['Speed'] = 500
+#     tmp_df_ko_500_shifted = shift_df(tmp_df_ko_500)
+
+#     tmp_df_ko_1000 = pd.read_excel(
+#         file_name, sheet_name=1, usecols='C:N', header=80, nrows=30)
+#     tmp_df_ko_1000['Label'] = 1
+#     tmp_df_ko_1000['Speed'] = 1000
+#     tmp_df_ko_1000_shifted = shift_df(tmp_df_ko_1000)
+
+#     df_data_org = pd.concat([tmp_df_ok_100, tmp_df_ok_500, tmp_df_ok_1000,
+#                             tmp_df_ko_100, tmp_df_ko_500, tmp_df_ko_1000], ignore_index=True)
+#     df_data_org_shifted = pd.concat([tmp_df_ok_100_shifted, tmp_df_ok_500_shifted, tmp_df_ok_1000_shifted,
+#                                     tmp_df_ko_100_shifted, tmp_df_ko_500_shifted, tmp_df_ko_1000_shifted], ignore_index=True)
+
+#     return df_data_org, df_data_org_shifted
 
 
 def data_visualization_2d_pca(x_clean_fill_nan, y, feature_list):
     ''' This function performs 2-d pca and visualize the result. '''
     # Use PCA to reduce the dimension to 2-D.
     pca = PCA(n_components=2)
-    principalComponents = pca.fit_transform(x_clean_fill_nan.loc[:, feature_list])
-    principalDf = pd.DataFrame(data=principalComponents, columns=['principal component 1', 'principal component 2'])
-    finalDf = pd.concat([principalDf, y], axis = 1)
+    principalComponents = pca.fit_transform(
+        x_clean_fill_nan.loc[:, feature_list])
+    principalDf = pd.DataFrame(data=principalComponents, columns=[
+                               'principal component 1', 'principal component 2'])
+    finalDf = pd.concat([principalDf, y], axis=1)
 
     print(finalDf.head())
     print(pca.explained_variance_ratio_)
 
     # Visualize the data.
-    fig = plt.figure(figsize = (8,8))
-    ax = fig.add_subplot(1,1,1) 
-    ax.set_xlabel('Principal Component 1', fontsize = 15)
-    ax.set_ylabel('Principal Component 2', fontsize = 15)
-    ax.set_title('2 component PCA', fontsize = 20)
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel('Principal Component 1', fontsize=15)
+    ax.set_ylabel('Principal Component 2', fontsize=15)
+    ax.set_title('2 component PCA', fontsize=20)
     targets = [False, True]
     colors = ['r', 'g']
     marker = ['*', 'o']
     alpha = [.3, .3]
     for target, color, marker, alpha in zip(targets, colors, marker, alpha):
         indicesToKeep = finalDf['Label'] == target
-        ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
-                , finalDf.loc[indicesToKeep, 'principal component 2']
-                , c = color
-                , s = 50, marker=marker, alpha=alpha)
+        ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1'], finalDf.loc[indicesToKeep,
+                   'principal component 2'], c=color, s=50, marker=marker, alpha=alpha)
     ax.legend(targets)
     ax.grid()
